4cca046a9acd1013d2582e3bacebaa50f1438279
Leonkazaryan <leon.kazaryan@nu.edu.kz>
2026-02-21 19:30:37 +0500

    Swagger

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Providers } from './app/providers'
+import { TwoGisSwagger } from './swagger/TwoGisSwagger'
 import './styles/globals.css'
+
+const isSwaggerRoute = window.location.pathname.startsWith('/swagger')
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Providers>
-      <App />
-    </Providers>
+    {isSwaggerRoute ? (
+      <TwoGisSwagger />
+    ) : (
+      <Providers>
+        <App />
+      </Providers>
+    )}
   </React.StrictMode>,
 )
